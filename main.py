1ac6a58dba305d0526fe28be49ae8da220832d35
anber <anberlik@gmail.com>
2021-08-03 22:01:42 +0300

    Добавил условие

--- main.py
+++ main.py
@@ -3,16 +3,28 @@ from random import randint
 
 data = getdata()
 for x in range(len(data)):
-    print(data[x].get('name'),)
+    print(data[x].get('name'), )
+
+A = data[randint(0, len(data))]
+B = data[randint(0, len(data))]
+count = 1
+reserverd_instagram = ''
+# for random_instagram in range(0, len(data)):
 
 
-#Выбираем 1 случайный аккаунт
-print(data[randint(0, len(data))])
 
-#Получаем 2ой случайных аккаунта для сравнения
+print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}.")
+print('___VS___')
+print(f"Against B: {B['name']}, a {B['description']}, from {B['country']}.")
 
-#Сравниваем 2 аккаунта
+while count > 0:
+    choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+    if choice == 'a' and A['follower_count'] >= B['follower_count']:
+        count += 1
+    elif choice == 'b' and A['follower_count'] <= B['follower_count']:
+        count += 1
+    else:
+        count = 0
+        print('Вы проиграли')
+    print(f'Ваш счет {count}')
 
-#Если не удагадали - проиграли
-
-#Если угадали - выводим значение которое угадали и сравниваем с новым значением по не проигрем
